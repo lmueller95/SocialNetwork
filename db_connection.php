66db4ecebf2d89f84faebdaf89db5ada1436a2fd
Lion <lion.mueller@newsfactory.de>
2022-07-14 16:45:33 +0200

    split code into own files for better readability

---
+++ db_connection.php
@@ -0,0 +1,12 @@
+<?php
+
+ob_start(); //Turns on output buffering
+session_start();
+
+$timezone = date_default_timezone_get("Europe/Berlin");
+$con = mysqli_connect("localhost", "azubis", "8PM-KKUi?KQJi%G*)KZP", "azubi_lion");
+if (mysqli_connect_errno()) {
+    echo "Failed to connect: " . mysqli_connect_errno();
+}
+
+?>
