6fa1069aefbbf24d0836b0a7c02eb4ec6f8d19b3
Lion <lion.mueller@newsfactory.de>
2022-07-14 15:42:07 +0200

    generation a unique username

--- register.php
+++ register.php
@@ -84,9 +84,18 @@ if (isset($_POST['register_button'])) {
     if (strlen($password > 30 || strlen($password) < 5)) {
         array_push($error_array, 'Your password must be betwen 5 and 30 characters<br>');
     }
-//    if (!empty($error_array)) {
-//        var_dump($error_array);
-//    }
+    if (empty($error_array)) {
+        $password = md5($password);
+        $username = strtolower($fname . "_" . $lname);
+        $check_username_query = mysqli_query($con, "SELECT username FROM Social WHERE username = '$username'");
+        $i = 0;
+        while (mysqli_num_rows($check_username_query) != 0) {
+            $i++;
+            $username = $username . "_" . $i;
+            $check_username_query = mysqli_query($con, "SELECT username FROM Social WHERE username = '$username'");
+        }
+        echo 'alles gut';
+    }
 }
 ?>
 
